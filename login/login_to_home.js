8c674ac4a6ded638da687431e13a4992f0cbb5d0
Hezkiel09 <ridhxrasyid@gmail.com>
2025-06-08 14:58:54 +0700

    update loginn

--- login/login_to_home.js
+++ login/login_to_home.js
@@ -2,16 +2,33 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form");
 
   form.addEventListener("submit", function (e) {
-    e.preventDefault(); // Mencegah reload halaman
+    e.preventDefault();
 
-    const email = document.getElementById("email").value.trim();
-    const password = document.getElementById("password").value.trim();
+    const emailInput = document.getElementById("email").value.trim();
+    const passwordInput = document.getElementById("password").value.trim();
 
-    if (email === "" || password === "") {
+    if (!emailInput || !passwordInput) {
       alert("Semua kolom harus diisi!");
-    } else {
+      return;
+    }
+
+    const storedUser = JSON.parse(localStorage.getItem("registeredUser"));
+
+    if (!storedUser) {
+      alert("Akun belum terdaftar!");
+      return;
+    }
+
+    if (
+      storedUser &&
+      storedUser.email === emailInput &&
+      storedUser.password === passwordInput
+    ) {
       alert("Login berhasil!");
-      window.location.href = "../home/home.html"; // Ganti sesuai lokasi file home
+      localStorage.setItem("isLoggedIn", "true");
+      window.location.href = "../home/home.html";
+    } else {
+      alert("Email atau password salah!");
     }
   });
 });
